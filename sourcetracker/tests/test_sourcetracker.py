f22dc1ff533d09ecb591442a9719704a8eb1b85b
Will Van Treuren <wdwvt1@gmail.com>
2016-09-06 16:57:48 -0700

    TST: Adds consistency tests of high-level functions (#67)

--- sourcetracker/tests/test_sourcetracker.py
+++ sourcetracker/tests/test_sourcetracker.py
@@ -23,7 +23,7 @@ from sourcetracker._sourcetracker import (biom_to_df,
                                           cumulative_proportions,
                                           single_sink_feature_table,
                                           ConditionalProbability,
-                                          gibbs_sampler)
+                                          gibbs_sampler, _gibbs, _gibbs_loo)
 
 
 class TestCheckAndCorrectData(TestCase):
@@ -785,10 +785,10 @@ class ConditionalProbabilityTests(TestCase):
         np.testing.assert_array_almost_equal(obs_jp, exp_jp)
 
 
-class TestGibbsDeterministic(TestCase):
+class TestGibbs(TestCase):
     '''Unit tests for Gibbs based on seeding the PRNG and hand calculations.'''
 
-    def test_single_pass(self):
+    def test_single_pass_gibbs_sampler(self):
         # The data for this test was built by seeding the PRNG, and making the
         # calculations that Gibb's would make, and then comparing the results.
         restarts = 1
@@ -872,6 +872,184 @@ class TestGibbsDeterministic(TestCase):
                                       seq_env_assignments)
         np.testing.assert_array_equal(obs_ta.squeeze()[order],
                                       expected_et_pairs[1, :])
+
+    def test_consistency_when_gibbs_seeded(self):
+        '''Test consistency of `_gibbs` from run to run.
+
+        Notes
+        -----
+        The number of calls to the PRNG should be stable (and thus this test,
+        which is seeded, should not fail). Any changes made to the code which
+        cause this test to fail should be scrutinized very carefully.
+
+        If the number of calls to the PRNG has not been changed, then an error
+        has been introduced somewhere else in the code. If the number of calls
+        has been changed, the deterministic tests should fail as well, but
+        since they are a small example they might not fail (false negative).
+        This test is extensive (it does 201 loops through the entire
+        `gibbs_sampler` block).
+        '''
+        features = ['o1', 'o2', 'o3', 'o4', 'o5', 'o6']
+        source1 = np.array([10, 10, 10, 0, 0, 0])
+        source2 = np.array([0, 0, 0, 10, 10, 10])
+        sink1 = .5*source1 + .5*source2
+        sinks = pd.DataFrame(sink1.reshape(1, 6).astype(np.int32),
+                             index=['sink1'], columns=features)
+        sources = pd.DataFrame(np.vstack((source1, source2)).astype(np.int32),
+                               index=['source1', 'source2'], columns=features)
+
+        np.random.seed(1042)
+        mpm, mps, fts = _gibbs(sources, sinks, alpha1=.001, alpha2=.01, beta=1,
+                               restarts=3, draws_per_restart=5, burnin=50,
+                               delay=4, cluster=None,
+                               create_feature_tables=True)
+
+        possible_sources = ['source1', 'source2', 'Unknown']
+        vals = np.array([[0.44, 0.44666667, 0.11333333]])
+        exp_mpm = pd.DataFrame(vals, index=['sink1'], columns=possible_sources)
+
+        vals = np.array([[0.00824322, 0.00435465, 0.01047985]])
+        exp_mps = pd.DataFrame(vals, index=['sink1'], columns=possible_sources)
+
+        vals = np.array([[69, 64, 65,  0,  0,  0],
+                         [0, 0,  0, 67, 70, 64],
+                         [6, 11, 10,  8, 5, 11]], dtype=np.int32)
+        exp_fts = pd.DataFrame(vals, index=possible_sources, columns=features)
+
+        pd.util.testing.assert_frame_equal(mpm, exp_mpm)
+        pd.util.testing.assert_frame_equal(mps, exp_mps)
+        pd.util.testing.assert_frame_equal(fts[0], exp_fts)
+
+    def test_consistency_when_gibbs_loo_seeded(self):
+        '''Test consistency of `_gibbs_loo` from run to run.
+
+        Notes
+        -----
+        The number of calls to the PRNG should be stable (and thus this test,
+        which is seeded, should not fail). Any changes made to the code which
+        cause this test to fail should be scrutinized very carefully.
+
+        If the number of calls to the PRNG has not been changed, then an error
+        has been introduced somewhere else in the code. If the number of calls
+        has been changed, the deterministic tests should fail as well, but
+        since they are a small example they might not fail (false negative).
+        This test is extensive (it does 201 loops through the entire
+        `gibbs_sampler` block for each source).
+        '''
+        source1a = np.array([10, 10, 10, 0, 0, 0])
+        source1b = np.array([8, 8, 8, 2, 2, 2])
+        source2a = np.array([0, 0, 0, 10, 10, 10])
+        source2b = np.array([4, 4, 4, 6, 6, 6])
+
+        vals = np.vstack((source1a, source1b, source2a,
+                          source2b)).astype(np.int32)
+        source_names = ['source1a', 'source1b', 'source2a', 'source2b']
+        feature_names = ['o1', 'o2', 'o3', 'o4', 'o5', 'o6']
+        sources = pd.DataFrame(vals, index=source_names, columns=feature_names)
+
+        np.random.seed(1042)
+        obs_mpm, obs_mps, obs_fts = _gibbs_loo(sources, alpha1=.001,
+                                               alpha2=.01, beta=1, restarts=3,
+                                               draws_per_restart=5, burnin=50,
+                                               delay=4, cluster=None,
+                                               create_feature_tables=True)
+
+        vals = np.array([[0., 0.62444444, 0., 0.01555556, 0.36],
+                         [0.68444444, 0., 0.09333333, 0.12666667, 0.09555556],
+                         [0., 0.00888889, 0., 0.08222222, 0.90888889],
+                         [0.19111111, 0.2, 0.5, 0., 0.10888889]])
+        exp_mpm = pd.DataFrame(vals, index=source_names,
+                               columns=source_names + ['Unknown'])
+
+        vals = np.array([[0., 0.02406393, 0., 0.0015956, 0.02445387],
+                         [0.0076923, 0., 0.00399176, 0.00824322, 0.00648476],
+                         [0., 0.00127442, 0., 0.00622575, 0.00609752],
+                         [0.00636175, 0.00786721, 0.00525874, 0., 0.00609752]])
+        exp_mps = pd.DataFrame(vals, index=source_names,
+                               columns=source_names + ['Unknown'])
+
+        fts0_vals = np.array([[0, 0, 0, 0, 0, 0],
+                              [93, 87, 101, 0, 0, 0],
+                              [0, 0, 0, 0, 0, 0],
+                              [3, 4, 0, 0, 0, 0],
+                              [54, 59, 49, 0, 0, 0]])
+        fts1_vals = np.array([[113, 98, 97, 0, 0, 0],
+                              [0, 0, 0, 0, 0, 0],
+                              [0, 0, 0, 15, 13, 14],
+                              [5, 7, 11, 11, 12, 11],
+                              [2, 15, 12, 4, 5, 5]])
+        fts2_vals = np.array([[0, 0, 0, 0, 0, 0],
+                              [0, 0, 0, 2, 1, 1],
+                              [0, 0, 0, 0, 0, 0],
+                              [0, 0, 0, 12, 12, 13],
+                              [0, 0, 0, 136, 137, 136]])
+        fts3_vals = np.array([[28, 27, 31, 0, 0, 0],
+                              [27, 24, 25, 3, 4, 7],
+                              [0, 0, 0, 80, 71, 74],
+                              [0, 0, 0, 0, 0, 0],
+                              [5, 9, 4, 7, 15, 9]])
+        fts_vals = [fts0_vals, fts1_vals, fts2_vals, fts3_vals]
+        exp_fts = [pd.DataFrame(vals, index=source_names + ['Unknown'],
+                   columns=feature_names) for vals in fts_vals]
+
+        pd.util.testing.assert_frame_equal(obs_mpm, exp_mpm)
+        pd.util.testing.assert_frame_equal(obs_mps, exp_mps)
+        for obs_fts, exp_fts in zip(obs_fts, exp_fts):
+            pd.util.testing.assert_frame_equal(obs_fts, exp_fts)
+
+    def test_gibbs_close_to_sourcetracker_1(self):
+        '''This test is stochastic; occasional errors might occur.
+
+        Notes
+        -----
+        This tests against the R-code SourceTracker version 1.0, using
+        R version 2.15.3.
+        '''
+
+        sources_data = \
+            np.array([[0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  4],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 0, 0, 0, 0,  0]],
+                     dtype=np.int32)
+        sources_names = ['source1', 'source2', 'source3']
+        feature_names = ['f%i' % i for i in range(32)]
+        sources = pd.DataFrame(sources_data, index=sources_names,
+                               columns=feature_names)
+
+        sinks_data = np.array([[0, 0, 0, 0, 0, 0, 170, 0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 385, 0, 0, 0, 0, 0, 0, 0, 350, 0, 0,
+                                0, 0, 95],
+                               [0, 0, 0, 0, 0, 0, 170, 0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 380, 0, 0, 0, 0, 0, 0, 0, 350, 0, 0,
+                                0, 0, 100],
+                               [0, 0, 0, 0, 0, 0, 170, 0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 378, 0, 0, 0, 0, 0, 0, 0, 350, 0, 0,
+                                0, 0, 102],
+                               [0, 0, 0, 0, 0, 0, 170, 0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 386, 0, 0, 0, 0, 0, 0, 0, 350, 0, 0,
+                                0, 0, 94]], dtype=np.int32)
+        sinks_names = ['sink1', 'sink2', 'sink3', 'sink4']
+        sinks = pd.DataFrame(sinks_data, index=sinks_names,
+                             columns=feature_names)
+
+        obs_mpm, obs_mps, _ = _gibbs(sources, sinks, alpha1=.001, alpha2=.1,
+                                     beta=10, restarts=2, draws_per_restart=2,
+                                     burnin=5, delay=2, cluster=None,
+                                     create_feature_tables=False)
+
+        exp_vals = np.array([[0.1695, 0.4781, 0.3497, 0.0027],
+                             [0.1695, 0.4794, 0.3497, 0.0014],
+                             [0.1693, 0.4784, 0.3499, 0.0024],
+                             [0.1696, 0.4788, 0.3494, 0.0022]])
+        exp_mpm = pd.DataFrame(exp_vals, index=sinks_names,
+                               columns=sources_names + ['Unknown'])
+
+        pd.util.testing.assert_index_equal(obs_mpm.index, exp_mpm.index)
+        pd.util.testing.assert_index_equal(obs_mpm.columns, exp_mpm.columns)
+        np.testing.assert_allclose(obs_mpm.values, exp_mpm.values, atol=.01)
 
 
 if __name__ == '__main__':
